9593d7e49c664e0a750cdea42ea0e2fcfe80b258
Ayush <a>
2021-07-03 17:29:32 +0530

    Add animationa and menu sidebar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Header.css';
+import CloseIcon from '@material-ui/icons/Close';
 
 function Header() {
-    return <div>Header</div>;
+    const [menuNavStatus, setMenuNavStatus] = useState(false);
+    return (
+        <div className='header'>
+            <a href='#a'>
+                <img src='/images/logo.svg' alt='' />
+            </a>
+            <div className='menu'>
+                <a href='#'>Model S</a>
+                <a href='#'>Model Y</a>
+                <a href='#'>Model 3</a>
+                <a href='#'>Model X</a>
+                <a href='#'>Solar Roof</a>
+                <a href='#'>Solar Panels</a>
+            </div>
+            <div className='rightMenu'>
+                <a href='#'>Shop</a>
+                <a href='#'>Account</a>
+                <a
+                    href='#a'
+                    onClick={() => {
+                        setMenuNavStatus(true);
+                    }}
+                >
+                    Menu
+                </a>
+            </div>
+            <div className={menuNavStatus ? 'menuNav' : 'menuNav menuClose'}>
+                <CloseIcon
+                    className='close'
+                    onClick={() => {
+                        setMenuNavStatus(false);
+                    }}
+                />
+                <li>
+                    <a href='#'>Model S</a>
+                </li>
+                <li>
+                    <a href='#'>Model X</a>
+                </li>
+                <li>
+                    <a href='#'>Existing Inventory</a>
+                </li>
+                <li>
+                    <a href='#'>Used Inventory</a>
+                </li>
+                <li>
+                    <a href='#'>Trade-In</a>
+                </li>
+                <li>
+                    <a href='#'>Cybertruck</a>
+                </li>
+                <li>
+                    <a href='#'>Roadster</a>
+                </li>
+            </div>
+        </div>
+    );
 }
 
 export default Header;
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import './Section.css';
+import Fade from 'react-reveal/Fade';
 
 function Section(props) {
     return (
         <div className={`wrap ${props.model}`}>
-            <div className='itemText'>
-                <h1>{props.title}</h1>
-                <p>{props.description}</p>
-            </div>
-            <div className='buttons'>
-                <div className='buttonGroup'>
-                    <div className='button'>{props.leftBtnText}</div>
-                    {props.rightBtnText && (
-                        <div className='button right'>{props.rightBtnText}</div>
-                    )}
+            <Fade bottom>
+                <div className='itemText'>
+                    <h1>{props.title}</h1>
+                    <p>{props.description}</p>
                 </div>
+            </Fade>
+            <div className='buttons'>
+                <Fade bottom>
+                    <div className='buttonGroup'>
+                        <div className='button'>{props.leftBtnText}</div>
+                        {props.rightBtnText && (
+                            <div className='button right'>
+                                {props.rightBtnText}
+                            </div>
+                        )}
+                    </div>
+                </Fade>
                 <img
                     src='/images/down-arrow.svg'
                     alt='down-arrow svg'
